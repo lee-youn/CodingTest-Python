1eb5825a711bf1e54d99e57db1a42f4f5aad8c18
lee-youn <5816654@naver.com>
2024-02-18 15:45:27 +0900

    CHAPTER04_구현

--- CHAPTER04_구현/4-3.py
+++ CHAPTER04_구현/4-3.py
@@ -1,18 +1,17 @@
-Y, X=map(str, input())
+# 4-3 왕실의 나이트
 
-#문자열로 입력받고, 아스키코드 값으로 변환
-x = ord(X)-48
-y = ord(Y)-96
+str_number = input()
+row= ord(str_number[0]) - 96
+column= int(str_number[1])
 
+move = [(1,2), (1,-2), (-1,2), (-1,-2), (2,1), (2,-1), (-2,1), (-2,-1)]
 count = 0
 
-#나이트가 이동할 수 있는 경우의 수
-motions = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (-1, 2), (1, -2), (-1, -2)]
-
-for move in motions:
-    x_move = x + move[0]
-    y_move = y + move[1]
-    if x_move > 0 and x_move <= 8 and y_move > 0 and y_move <= 8: count += 1
-
+for i in move:
+    move_column = i[0]
+    move_row = i[1]
+    if (row + move_row) >= 1 and (row + move_row) <= 8:
+        if (column+ move_column) >= 1 and (column+ move_column) <= 8:
+            count += 1
 
 print(count)
