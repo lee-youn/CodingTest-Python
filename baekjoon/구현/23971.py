5c189f6b6a600d12978032541a6858a6ed1ced4a
lee-youn <5816654@naver.com>
2026-01-05 15:25:53 +0900

    feat: 23971번 문제해결

---
+++ baekjoon/구현/23971.py
@@ -0,0 +1,9 @@
+﻿# 23971 ZOAC 4 문제
+def solution (h, w, n, m):
+    down = (h+n+1-1) // (n+1)
+    right = (w+m+1-1) // (m+1)
+
+    return down * right
+
+h, w, n, m = map(int, input().split())
+print(solution(h,w,n,m))
