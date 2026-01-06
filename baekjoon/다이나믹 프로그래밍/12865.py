c06473bcd3f90b8b1b608848bb2dc4e14660937f
lee-youn <5816654@naver.com>
2026-01-06 12:56:29 +0900

    feat: 12865 문제풀이

---
+++ baekjoon/다이나믹 프로그래밍/12865.py
@@ -0,0 +1,18 @@
+﻿#12865번 평범한 배낭 문제 해결
+import sys
+input = sys.stdin.readline
+
+def solution():
+    N, K = map(int,input().split())
+    dp = [0] * (K+1)
+
+    for _ in range(N):
+        W, V = map(int, input().split())
+
+        for i in range(K, W-1, -1):
+            dp[i] = max(dp[i], dp[i-W]+V)
+    print(max(dp))
+    return
+
+if __name__ == "__main__":
+    solution()
