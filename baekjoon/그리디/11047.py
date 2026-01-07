2a5fbc0d3f89a1f5a72dba9a10c512fa21e7fc9d
lee-youn <5816654@naver.com>
2026-01-07 11:07:18 +0900

    feat: 11047 문제 풀이

---
+++ baekjoon/그리디/11047.py
@@ -0,0 +1,15 @@
+﻿# 동전 0 문제 풀이
+N, K = map(int, input().split())
+coins = [int(input()) for i in range(N)]
+print(coins)
+
+coins = sorted(coins, reverse=True)
+
+count = 0
+print(coins)
+
+for i in coins:
+    count += K // i
+    K = K % i
+
+print(count)
