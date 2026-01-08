aa4782410321a7bc71b7b5284d10db3c8d31d232
lee-youn <5816654@naver.com>
2026-01-08 11:00:52 +0900

    feat: 2437번 문제 해결

--- baekjoon/그리디/11047.py
+++ baekjoon/그리디/11047.py
@@ -1,12 +1,10 @@
 ﻿# 동전 0 문제 풀이
 N, K = map(int, input().split())
 coins = [int(input()) for i in range(N)]
-print(coins)
 
 coins = sorted(coins, reverse=True)
 
 count = 0
-print(coins)
 
 for i in coins:
     count += K // i
---
+++ baekjoon/그리디/2437.py
@@ -0,0 +1,26 @@
+﻿# N = int(input())
+# weight =list(map(int, input().split()))
+# sorted_weight = sorted(weight, reverse=True)
+
+# for i in range(len(sorted_weight)):
+#     remain_sorted_weight = sum(sorted_weight[i+1:])
+#     if (sorted_weight[i] <= remain_sorted_weight) and remain_sorted_weight in sorted_weight:
+#         continue
+#     else:
+#         print(remain_sorted_weight + 1)
+#         break
+
+# sum을 매번하는 것은 효율에 안좋음 0(N^2)
+
+N = int(input())
+weights = list(map(int, input().split()))
+weights.sort() 
+
+reach = 0
+for i in weights:
+    if i > reach + 1:
+        break
+    else:
+        reach += i
+
+print(reach + 1)
